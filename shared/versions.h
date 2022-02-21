1eee923bcff85a59f2e91c77665bb05a16ac859a
Sajid Anwar <sajidanwar94@gmail.com>
2022-02-21 14:06:56 -0700

    [all] Use macro to define microcontroller version

--- shared/versions.h
+++ shared/versions.h
@@ -1,10 +1,17 @@
 #ifndef SHARED_VERSIONS_H
 #define SHARED_VERSIONS_H
 
+#define STRINGIFY(X) STRINGIFY_(X)
+#define STRINGIFY_(X) #X
+
+#ifndef PLATFORM_MICROCONTROLLER
+#define PLATFORM_MICROCONTROLLER Unknown
+#endif
+
 #include <stdint.h>
 
 static const uint16_t VERSION_FIRMWARE = 0xC2FC;
-static const char VERSION_MICROCONTROLLER[32] = "PIC18F46K42";
+static const char VERSION_MICROCONTROLLER[32] = STRINGIFY(PLATFORM_MICROCONTROLLER);
 static const uint16_t VERSION_CONFIGURATION = 0x01;
 static const uint16_t VERSION_BOOTLOADER = 0x02;
 
