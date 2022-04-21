f6247e84dafe96e57460e31b93c6064265a89ebc
Sajid Anwar <sajidanwar94@gmail.com>
2022-04-21 06:58:20 -0600

    [firmware] Fix packet finalize handling

--- firmware/lib/PS2Plus/commands/packet.c
+++ firmware/lib/PS2Plus/commands/packet.c
@@ -16,7 +16,7 @@ void command_packet_initialize(volatile command_packet *packet, command_packet_w
 void command_packet_step(volatile command_packet *packet, volatile controller_state *state, uint8_t command_byte) {
   packet->command_byte = command_byte;
 
-  if (!packet->ignore) {
+  if (!packet->completed && !packet->ignore) {
     if (packet->packet_index == 0) {
       packet->write(state->mode);
 
