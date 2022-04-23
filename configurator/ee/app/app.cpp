c170c338c2927dffa70d24e58b70e272db83022d
Sajid Anwar <sajidanwar94@gmail.com>
2022-04-23 10:21:56 -0600

    [configurator] Remove unused app code

--- configurator/ee/app/app.cpp
+++ configurator/ee/app/app.cpp
@@ -58,12 +58,6 @@ void app_ps2plus_ports_display(ImGuiIO &io, configurator_state *state) {
 }
 
 void app_display(ImGuiIO &io, configurator_state *state) {
-    // // Whether the user is selecting a section in the left pane
-    // static bool is_selecting_section = false;
-
-    // // Actively displayed section content
-    // static int selected_content = 0;
-
     static int active_tab = 0;
     static int tab_count = 5;
 
@@ -140,87 +134,6 @@ void app_display(ImGuiIO &io, configurator_state *state) {
             ImGui::EndTabBar();
         }
         ImGui::PopStyleVar(/* ImGuiStyleVar_FramePadding */);
-
-
-
-        // if (is_selecting_section && !dialog_displaying) {
-        //     // Only allow the left pane to have focus while the user selects
-        //     ImGui::SetNextWindowFocus();
-        // }
-        // ImGui::BeginChild("List", ImVec2(150, 0), true);
-        // {
-        //     if (ImGui::Selectable("Information", !is_selecting_section && selected_content == 0, 0, ImVec2(0, 25))) {
-        //         selected_content = 0;
-        //         is_selecting_section = false;
-        //     }
-        //     if (ImGui::Selectable("Configuration", !is_selecting_section && selected_content == 1, 0, ImVec2(0, 25))) {
-        //         selected_content = 1;
-        //         is_selecting_section = false;
-        //     }
-        //     if (ImGui::Selectable("Firmware", !is_selecting_section && selected_content == 2, 0, ImVec2(0, 25))) {
-        //         selected_content = 2;
-        //         is_selecting_section = false;
-        //     }
-        //     if (ImGui::Selectable("About", !is_selecting_section && selected_content == 3, 0, ImVec2(0, 25))) {
-        //         selected_content = 3;
-        //         is_selecting_section = false;
-        //     }
-        // }
-        // ImGui::EndChild();
-
-        // ImGui::SameLine();
-        // ImGui::BeginGroup();
-        // {
-        //     // Content pane will retain focus if it is selected
-        //     if (selected_content >= 0 && !is_selecting_section && !dialog_displaying) {
-        //         ImGui::SetNextWindowFocus();
-        //     }
-
-        //     app_ps2plus_ports_display(io, state);
-
-        //     // Content pane has full height, less the height of a single frame which will be used to
-        //     // display contextual button controls
-        //     ImGui::BeginDisabled(is_selecting_section);
-        //     ImGui::BeginChild("ChildSection", ImVec2(0, -ImGui::GetFrameHeightWithSpacing()), true);
-        //     {
-        //         if (selected_content == 0) {
-        //             app_section_information(io, state);
-        //         }
-
-        //         if (selected_content == 1) {
-        //             app_section_configuration(io, state);
-        //         }
-
-        //         if (selected_content == 2) {
-        //             app_section_firmware(io, state);
-        //         }
-                
-        //         if (selected_content == 3) {
-        //             app_section_about(io, state);
-        //         }
-
-        //         // Allow the user to change sections using the triangle button
-        //         if (!IsGamepadCaptured() && !dialog_displaying && (state->pad_status.IsButtonPressed(PAD_TRIANGLE))) {
-        //             is_selecting_section = !is_selecting_section;
-        //         }
-
-        //         // Dim this content section window if the user is selecting a different section
-        //         if (is_selecting_section) {
-        //             PS2Plus::UI::WindowOverlay(0.6f);
-        //         }
-        //     }
-        //     ImGui::EndChild();
-        //     ImGui::EndDisabled();
-
-        //     // Bottom row with control information
-        //     {
-        //         PS2Plus::UI::GamePadIcon(PS2Plus::UI::WidgetGamePadIconType_Triangle);
-        //         ImGui::SameLine();
-        //         ImGui::Text("Change Section");
-        //         ImGui::SameLine();
-        //     }
-        // }
-        // ImGui::EndGroup();
     }
     ImGui::End();
 }
