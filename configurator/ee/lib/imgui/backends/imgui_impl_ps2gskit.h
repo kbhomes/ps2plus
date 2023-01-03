bbe426750f4c939768eb3b84cbdbc1df4c176e91
Sajid Anwar <sajidanwar94@gmail.com>
2023-01-03 16:19:33 -0700

    [configurator] Upgrade dear imgui to v1.89.1

--- configurator/ee/lib/imgui/backends/imgui_impl_ps2gskit.h
+++ configurator/ee/lib/imgui/backends/imgui_impl_ps2gskit.h
@@ -1,11 +1,6 @@
 // dear imgui: Renderer Backend for PS2 gsKit
 // This needs to be used along with the PS2SDK backend
 
-// You can use unmodified imgui_impl_* files in your project. See examples/ folder for examples of using this. 
-// Prefer including the entire imgui/ repository into your project (either as a copy or as a submodule), and only build the backends you need.
-// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
-// Read online: https://github.com/ocornut/imgui/tree/master/docs
-
 #pragma once
 #include "../imgui.h"      // IMGUI_IMPL_API
 #include <gsKit.h>
